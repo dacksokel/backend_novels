9cae7074d638c25736b45e50ec7a7e6aa1fb09d3
Jhony M. Navarro M <dacksokel@gmail.com>
2023-06-10 15:31:47 -0400

    update the get novels by paginated in services

--- src/controllers/NovelsControllers.ts
+++ src/controllers/NovelsControllers.ts
@@ -1,5 +1,5 @@
 import e, { NextFunction, Request, Response } from 'express';
-import NovelModel from '../models/NovelsModels';
+import INovel from '../interfaces/INovel';
 import NovelsServices from '../services/NovelsServices';
 
 class NovelsControllers {
@@ -8,12 +8,12 @@ class NovelsControllers {
     res.status(200).json({ status: 'OK' });
   }
 
-  async saveNovelController (req: Request, res: Response) {
-    const novel = (req as any).body;
+  async saveNovelController(req: Request, res: Response) {
+    const novel = req.body as INovel;
     const responseSaveNovel = await NovelsServices.saveNovelService(novel);
     if (responseSaveNovel) {
       res.status(200).json({ status: 'Success' });
-    }else {
+    } else {
       res.status(500).json({ status: 'Error saving novel' });
     }
 
@@ -22,31 +22,12 @@ class NovelsControllers {
   async getNovelsPaginated(req: Request, res: Response) {
     const page: number = parseInt(req.query.page as string) || 1;
     const limit: number = parseInt(req.query.limit as string) || 100;
-    const startIndex: number = (page - 1) * limit;
-    const endIndex: number = page * limit;
+    const response = await NovelsServices.getNovelsPaginatedServices(page, limit);
 
-    const results: any = {};
-    const count = await NovelModel.countDocuments();
-
-    if (endIndex < count) {
-      results.next = {
-        page: page + 1,
-        limit: limit
-      };
-    }
-
-    if (startIndex > 0) {
-      results.previous = {
-        page: page - 1,
-        limit: limit
-      };
-    }
-
-    try {
-      results.results = await NovelModel.find().limit(limit).skip(startIndex).exec();
-      res.status(200).json(results);
-    } catch (e: any) {
-      res.status(500).json({ message: e.message });
+    if (response) {
+      res.status(200).json(response);
+    } else {
+      res.status(500).json({ message: response.message });
     }
 
   }
--- src/services/NovelsServices.ts
+++ src/services/NovelsServices.ts
@@ -1,8 +1,9 @@
 import NovelModel from '../models/NovelsModels';
+import INovel from '../interfaces/INovel';
 
 class NovelsServices {
 
-    async saveNovelService (novel: any): Promise<boolean> {
+    async saveNovelService(novel: INovel): Promise<boolean> {
         const saveNovel = new NovelModel(novel);
         try {
             console.log(novel);
@@ -10,6 +11,36 @@ class NovelsServices {
             return true;
         } catch (error) {
             return false;
+        }
+    }
+
+    async getNovelsPaginatedServices(page: number, limit: number) {
+        console.log(` page: ${page} limit: ${limit}`);
+        const startIndex: number = (page - 1) * limit;
+        const endIndex: number = page * limit;
+
+        try {
+            const results: any = {};
+            const count = await NovelModel.countDocuments();
+            console.log(`this it's the count novels: ${count}`);
+            if (endIndex < count) {
+                results.next = {
+                    page: page + 1,
+                    limit: limit
+                };
+            }
+
+            if (startIndex > 0) {
+                results.previous = {
+                    page: page - 1,
+                    limit: limit
+                };
+            }
+            const novelsReslts = await NovelModel.find().limit(limit).skip(startIndex).exec();
+            results.results = novelsReslts;
+            return results;
+        } catch (e: any) {
+            return e;
         }
     }
 
